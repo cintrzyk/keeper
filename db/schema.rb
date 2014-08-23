bdd943bc63010d4d34d2a68e8ae0a3346b73d4d5
Marcin Brzeziński <cintrzyk@gmail.com>
2014-08-23 22:57:39 +0200

    Item model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140823183638) do
+ActiveRecord::Schema.define(version: 20140823205600) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: true do |t|
+    t.integer  "user_id"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "items", ["user_id"], name: "index_items_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email"
